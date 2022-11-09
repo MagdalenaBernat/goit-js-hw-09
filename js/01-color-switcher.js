e77c9633add669d6614465c1c06a02331bd245fa
MagdalenaBernat <magdalena.bernat@onet.pl>
2022-11-09 01:08:30 +0100

    update, colorSwitcher

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,1 +1,19 @@
+"use strict";
 
+const startBtn = document.querySelector('button[type="button"]data-start');
+const stopBtn = document.querySelector('button[type="button"]data-stop');
+    
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let myTimer; 
+
+startBtn.addEventListener("click", () => {
+    myTimer = setTimeout(getRandomHexColor, 1000);
+    startBtn.disabled = true;
+});
+
+stopBtn.addEventListener("click", () => {
+    clearTimeuot(myTimer);
+});
--- js/03-promises.js
+++ js/03-promises.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
