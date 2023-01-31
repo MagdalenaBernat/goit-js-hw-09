5b677e5a26498c23d06de1c886cc9092c30dc881
MagdalenaBernat <104648579+MagdalenaBernat@users.noreply.github.com>
2023-01-31 19:01:22 +0000

    Deploying to gh-pages from @ MagdalenaBernat/goit-js-hw-09@7e924cbb8990235cecfa2c086af595cfb521d08b 🚀

---
+++ 01-color-switcher.e13e35ea.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=document.querySelector("body");e.addEventListener("click",(function(){t=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),e.disabled=!0,n.disabled=!1}),1e3)})),n.addEventListener("click",(function(){clearInterval(t);e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.e13e35ea.js.map
---
+++ 03-promises.5ee98fbd.js
@@ -0,0 +1,2 @@
+!function(){"use strict"}();
+//# sourceMappingURL=03-promises.5ee98fbd.js.map
