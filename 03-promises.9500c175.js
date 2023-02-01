44af7f6d142d8683c5483aff4fde70f38a224cba
MagdalenaBernat <104648579+MagdalenaBernat@users.noreply.github.com>
2023-02-01 15:14:17 +0000

    Deploying to gh-pages from @ MagdalenaBernat/goit-js-hw-09@62d920a93c0d6f893b403f9faefec135d7386abb 🚀

--- 03-promises.9500c175.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";(void Math.random()).then((function(o){var n=o.position,c=o.delay;console.log("✅ Fulfilled promise ".concat(n," in ").concat(c,"ms"))})).catch((function(o){var n=o.position,c=o.delay;console.log("❌ Rejected promise ".concat(n," in ").concat(c,"ms"))}))}();
-//# sourceMappingURL=03-promises.9500c175.js.map
